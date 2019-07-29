4d3bcbd6811af193c857fc549b73294d11d23ed2
fenegz <f3n3gz@gmail.com>
2019-07-29 11:54:46 +0500

    lesson1

---
+++ annotationConfig/src/main/java/annotationTest/Camera.java
@@ -0,0 +1,9 @@
+package annotationTest;
+
+public interface Camera {
+    CameraRoll getCameraRoll();
+
+    void setCameraRoll(CameraRoll cameraRoll);
+
+    void doPhotograph();
+}
---
+++ javaConfig/src/main/java/App.java
@@ -0,0 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
+        Camera camera=context.getBean("camera",Camera.class);
+        camera.doPhotograph();
+
+    }
+}
